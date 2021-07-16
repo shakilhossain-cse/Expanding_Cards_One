7da29344275108c378101cd90885acb33922343b
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-07-16 21:54:52 +0600

    add class and remove css by js done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// get all cards 
+const cards = document.querySelectorAll('.card');
+// use for each loop in  for get all div 
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        // remove active class function call 
+        removeActiveClasses()
+        // and add active class 
+        card.classList.add('active')
+    })
+})
 
 
-
+function removeActiveClasses() {
+    cards.forEach(card => {
+        card.classList.remove('active');
+    })
+}
